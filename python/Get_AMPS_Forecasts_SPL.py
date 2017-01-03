cbf8a99559232c1aa7b334b9e431d206c538c708
markd <mark.dalberth@gmail.com>
2017-01-02 17:48:09 -0700

    First PAsite commit

---
+++ python/Get_AMPS_Forecasts_SPL.py
@@ -0,0 +1,213 @@
+#!/usr/bin/python3.5
+# -*- coding: utf-8 -*-
+
+"""
+Created on Mon Feb 15 08:50:40 2016
+
+@author: Mark Dalberth
+
+This script will download forecasts from the Antarctic Mesoscale Prediction System's
+website and save them into the folder e:\AMPSForecasts. Three different grids are
+downloaded - 3km, 9km, and 10km. These are then attached to an email and sent to an
+email list. The list is in the variable 'toaddr' down below.
+
+I let this run for a while and sometimes the 10km forecasts would not be complete
+when the email went out. I asked Kevin Manning at ucar, and he said that there is
+a file on the website (METEOGRAMS_ARE_DONE) that will tell you if the meteograms are ready.
+I rewrote the script to look for that file.
+
+This version of the script is meant to be started by the Task Scheduler and then
+the timing is done by the script itself. This eliminates the need for an external log file
+that keeps track of the number of iterations. Also, this way the script can just
+end when the files are downloaded.
+
+IMPORTANT: In Pythonanywhere, you should put #!/usr/bin/python3.5 in the first line to
+ensure that the correct version of python is being run. I had lots of problems because
+pandas functions change from version to version. If I used python 2.7 (the default, i think)
+the script would not import data correctly. Ugh.
+"""
+#The 'requests' module can read url's
+import requests
+#datetime is used to get dates in various formats
+import datetime
+#The time module is used for its 'sleep' function.
+import time
+import spltable
+
+#This is the path where the forecasts will be saved.
+#path = "C:\\Users\\owner\\Documents\\website\\PAsite20170102\\static\\images\\"
+path = '/home/markd/www/static/images/'
+#this is the network drive
+#networkdrive = '\\\\edisto\\charts\\'
+
+
+
+#This version of the script replaces the log file with a list (called log).
+#Use the time module to time the delay between download attempts.
+log = [0,404]
+
+#maxiteration is the max number of times the script will loop.
+maxiteration = 10
+#delay is the length in seconds that the script will wait before trying again.
+delay = 600
+
+#Print this statement so that people looking at PAWS will know why the
+#script window is open
+print('''This is the script that retrieves the AMPS forecasts for South Pole. It checks for the \n\r\
+meteograms to be complete and then downloads them.\n\n\r\
+It will exit after %s minutes if no meteograms are found.\n\n\r''' % ((maxiteration+1)*delay/60))
+
+#404 means that the web page was not found.
+while log[1] == 404 :
+
+    #If log[1] is '404' then either this is the first time
+    #the script has been run today or the the METOGRAMS_ARE_DONE file
+    #did not exist the last time we looked.
+
+    print('Looking to see if 10 km meteograms are done...')
+    #Next, I generate two different dates. today1 is used in the url for the forecasts.
+    #today2 is used in the text of the email message.
+    today1 = datetime.datetime.utcnow().strftime('%Y%m%d')
+    today2 = datetime.date.today().isoformat()
+    #The following three lines are for testing purposes...
+#    testtime = datetime.datetime.utcnow()-datetime.timedelta(hours=16)
+#    today1 = testtime.strftime('%Y%m%d')
+#    today2 =  testtime.strftime('%Y-%m-%d')
+
+
+
+    #Use the url below to check if the meteograms are ready. The 10 km one takes the
+    #longest out of the 3km, 9km and 10km. Look for the file METEOGRAMS_ARE_DONE
+    url_10km_exist = 'http://www2.mmm.ucar.edu/rt/amps/wrf_plots/'+today1+'00/10km/meteogram/METEOGRAMS_ARE_DONE'
+    r_exists = requests.get(url_10km_exist)
+
+    #If r_exists.status_code == 200 then file METEOGRAMS_ARE_DONE exists.
+    #We can download the image files
+    if r_exists.status_code == 200 :
+        print('Meteograms are done. Download files...')
+        #set log[1] = 200 to stop the while loop
+        log[1] = 200
+        log[0] += 1
+
+        #subprocess.call will be used to copy the images onto the network
+        #from subprocess import call
+
+#        #Now, import the modules needed to send the email
+#        #smtplib actually sends the email
+#        import smtplib
+#        #The next three MIME packages are used to construct the email.
+#        from email.mime.multipart import MIMEMultipart
+#        from email.mime.text import MIMEText
+#        from email.mime.image import MIMEImage
+
+        #create the function 'get_image'....
+        def get_image(url, file):
+            "function to go to a url for an image file and save the image to the provided filename"
+            r = requests.get(url)
+            #Create the file that will hold the image and then save the image into the file
+            imagefile = open(file, 'wb')
+            for chunk in r.iter_content(100000):
+                imagefile.write(chunk)
+            imagefile.close()
+
+
+#        # Set up some variables used in the script...
+#        mailserver = 'mail.palmer.usap.gov'
+#        fromaddr = 'noreply@usap.gov'
+#        toaddr = ['pal.ra@usap.gov',
+#            'nandor.kovats.contractor@usap.gov',
+#            'PalmerMarineTechnician@usap.gov',
+#            'Palmer.CommsTech@usap.gov',
+#            'farrysc@hotmail.com',
+#            'BPCFX7@gmail.com',
+#            'mcateecarrie@gmail.com',
+#            'erin.p.pickett@gmail.com',
+#            'logan.pallin@gmail.com',
+#            'ryanyoung1@usf.edu',
+#            'bjbaker@usf.edu',
+#            'mamsler@uab.edu']
+
+        #Set up the urls for the 3km, 9km and 10km grid forecasts, and the filenames to store the .png's
+        url_3km = 'http://www2.mmm.ucar.edu/rt/amps/wrf_plots/' + today1 +'00/3km/meteogram/spl.png'
+        forecast_3km = 'spl_3km.png'
+
+        url_10km = 'http://www2.mmm.ucar.edu/rt/amps/wrf_plots/'+today1+'00/10km/meteogram/spl.png'
+        forecast_10km = 'spl_10km.png'
+
+        print('Getting the images...')
+        get_image(url_3km, path+forecast_3km)
+        get_image(url_10km, path+forecast_10km)
+        print('Done.')
+
+        tbl_file_10km = today1 + '00.10km.spl.table.txt'
+        url_10km_tbl = 'http://www2.mmm.ucar.edu/rt/amps/wrf_plots/'+today1+'00/10km/ts_text/'+tbl_file_10km
+
+        r = requests.get(url_10km_tbl)
+
+        txt_file = open(path+'10km.spl.table.txt', 'w')
+        txt_file.write(r.text)
+        txt_file.close()
+
+#        #save the .png files to edisto\charts (z:\) so they will appear on the intranet.
+#        call(['copy', path+'AMPS*.png', networkdrive], shell = True)
+#
+#        #Construct the email message...
+#        msg = MIMEMultipart()
+#        msg['From'] = fromaddr
+#        msg['To'] = ', '.join(toaddr)
+#        msg['Subject'] = 'AMPS Forecast for ' + today2
+#
+#        body = "Here are the AMPS forecasts generated for 12:00 UTC " + today2 + \
+#        ".\r\nThe 3km forecast goes out 39 hours, 9km 3 days, and 10km 5 days.\n\n\rTalk to the RA to be removed from this email list (pal.ra@usap.gov)." + \
+#        "\n\n\rThese will be emailed once per day when the calculations for the 12:00 UTC forecast are complete."
+#        msg.attach(MIMEText(body, 'plain'))
+#
+#        #Read the image for 3km meteogram into 'img', add the header (this sets the filename
+#        #of the attachment), then attach img to the message.
+#        with open(path+forecast_3km, 'rb') as fp:
+#            img = MIMEImage(fp.read())
+#        img.add_header('Content-Disposition', 'attachment', filename = forecast_3km)
+#        msg.attach(img)
+#
+#        #Repeat for 9km forecast
+#        with open(path+forecast_9km, 'rb') as fp:
+#            img = MIMEImage(fp.read())
+#        img.add_header('Content-Disposition', 'attachment', filename = forecast_9km)
+#        msg.attach(img)
+#
+#        #Repeat for the 10km meteogram
+#        with open(path+forecast_10km, 'rb') as fp:
+#            img = MIMEImage(fp.read())
+#        img.add_header('Content-Disposition', 'attachment', filename = forecast_10km)
+#        msg.attach(img)
+#
+#        #Now, send the email that was just constructed...
+#        print('Sending email...')
+#        server = smtplib.SMTP(mailserver)
+#        server.send_message(msg)
+#        server.quit()
+#        print('Done.')
+#
+#        #Run Get_AMPS_charts.py. This retrieves the weather charts and makes a movie from them.
+#        #The exec function executes a series of pythons statements, so this command
+#        #opens the file 'Get_AMPS_Charts.py', reads it and then executes the
+#        #statements.
+#        exec(open('e:\\scripts\\Python\\Get_AMPS_Charts.py').read())
+
+    elif r_exists.status_code == 404:
+        print('Meteograms are not done. Wait %s minutes. \titeration= %s\tstatus= %s' % (delay/60, log[0], log[1]))
+        #Increment the counter in log
+        if log[0] == maxiteration :
+            print('The 10 km meteogram is not done after %s iterations. Ending the loop.' % (maxiteration+1 ))
+            break
+        #increment the counter in the variable log for the next iteration.
+        log[0] += 1
+        time.sleep(delay)
+
+print('log[1] = ', log[1])
+
+#If the loop completed and data was downloadd, make the plot of the table data.
+if log[1] == 200:
+    print('Creating NZ time SPL plot....')
+    spltable.tbl_plot(path, '10km.spl.table.txt', today1)
+    print('Done.')
---
+++ python/mcmtable.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3.5
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  3 15:31:52 2016
+
+@author: Mark D
+
+This program gets the data table from the AMPS website for the MCM 5 day
+forecast. It plots the data using timestamps. Once the data is plotted the
+timestamps are converted to New Zealand time.
+
+The method used to convert the timestamps to a date in the NZ timezone uses
+the function Date_fmt and matplotlib's ticker.FuncFormatter. I could not
+get the DateFormatter method to work. Al times were in UTC even if I set the
+timezone.
+"""
+
+import pandas as pd
+from matplotlib import pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import datetime as dati
+import pytz
+from pytz import timezone #This is for convenience
+
+pi = np.pi
+
+#This is the function used to convert the x-axis timestamps to NZ timezone
+def Date_fmt(dt,x=None):
+    return dati.datetime.fromtimestamp(\
+        dt, tz = timezone('Pacific/Auckland')).strftime('%Y-%m-%d %H:%M')
+
+#This function converts Altimeter setting to pressure using the formula from
+#NOAA (http://www.srh.noaa.gov/images/epz/wxcalc/stationPressure.pdf). It
+#takes height in meters, and altimeter setting in inHg. I convert that to
+#mbar using that factor of 33.8639.
+def P(height, altim):
+    return 33.8639*altim*((288-height*0.0065)/288)**(5.2561)
+
+def tbl_plot(path,file,today):
+    '''Function used to plot the McM wx table scraped off AMPS site.'''
+
+    #print('pandas version: ', pd.__version__)
+    wx = pd.read_csv(path+file, sep = '\s+',\
+    skiprows = 3, engine = 'python', skipfooter = 7,\
+    header = 0)
+    print()
+
+    wx = wx.iloc[1:,:]
+
+    wx = wx.rename(columns = {'FCST': 'FCST HR'})
+
+    wx['FCST HR'] = wx['FCST HR'].astype(float)
+    wx['T'] = wx['T'].astype(float)
+#The dewpoint temp from the table is frequently above the predicted
+#air temp. This is not the case for the 'regular' 5 day forecast, and
+#I also don't believe that they get dew that frequently at McM. I'm
+#not going to plot this.
+#    wx['Td'] = wx['Td'].astype(float)
+    wx['Spd'] = wx['Spd'].astype(float)
+    wx['Dir'] = wx['Dir'].astype(float)
+    wx['Gust'] = wx['Gust'].astype(float)
+    wx['Altim'] = wx['Altim'].astype(float)
+
+    #Here is my solution to plotting the graphs in NZ time (i.e. tz =
+    #'Pacific/Auckland'). I will take the time in UTC and convert to timestamp
+    #using the datetime.timestamp() function. The resulting plot will have an
+    #x-axis with time since 1-1-1970 00:00
+    #
+    #Create a function that will convert the timestamp to a datetime with the
+    #New Zealand timezone. The function just uses datetime.fromtimestamp() -
+    #fromtimestamp alows you to speify the timezone. You
+    #then use that function in matplotlib's ticker.FuncFormatter() to give the
+    #correct format to the x-axis dates. This works.
+    #
+    #I tried using matplotlib's DateFormatter, but, although you can enter the
+    #timezone, the result is always in UTC. Is this another bug in handling
+    #timezones?
+
+    #Used the next 2 lines during testing
+    #today = '2016-09-03'
+    #today_utc = dati.datetime.strptime(today, '%Y-%m-%d')
+
+    today_utc = dati.datetime.strptime(today, '%Y%m%d')
+    today_utc = today_utc.replace(tzinfo = pytz.utc) #only use replace with utc!!
+    today_tstamp = today_utc.timestamp()
+
+    #Make a list of timestamps each element is today's timestamp. This will
+    #be used with the forecast hour from the AMPS table to create the list
+    #of timestamps for the plot.
+    tstamp_x = [today_tstamp]*41
+    for i, stamp in enumerate(tstamp_x):
+        tstamp_x[i] = today_tstamp + wx['FCST HR'].iloc[i]*3600
+
+
+    #Calculate the vector components of the wind velocity
+    U = -wx['Spd']*np.sin(wx['Dir']*pi/180)
+    V = -wx['Spd']*np.cos(wx['Dir']*pi/180)
+
+    #Set up the fig using subplots. The top graph is for wind the bottom is for T
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex = True)
+    ax2_P = ax2.twinx()
+
+    ax1.plot(tstamp_x, wx['Spd'])
+
+    #Read the y limits from the wind speed plot to find where to put the
+    #wind barbs on the Y-axis.
+    y_rng = ax1.get_ylim()[1] - ax1.get_ylim()[0]
+    Y = np.ones(41)*0.75*y_rng
+
+    #Plot the barbs
+    ax1.barbs(tstamp_x, Y, U, V, fill_empty = True, length = 6,\
+    rounding = True, flip_barb = True)
+
+    ax2.plot(tstamp_x, wx['T'])
+    ax2_P.plot(tstamp_x, P(6, wx['Altim']), 'r')
+
+    x_fmt = mpl.ticker.FuncFormatter(Date_fmt)
+    ax1.get_xaxis().set_major_formatter(x_fmt)
+
+    ax1.set_xlim(left=tstamp_x[0], right=tstamp_x[-1])
+    xtick_arr = np.arange(tstamp_x[0], tstamp_x[-1]+12*3600, 24*3600)
+    ax1.set_xticks(xtick_arr)
+    ax1.set_ylabel('Wind Sp (kn)')
+    ax2.set_ylabel('Temp (C)')
+    ax2.legend('T', loc = 'lower left')
+    ax2_P.set_ylabel('P (mbar)')
+    ax2_P.legend('P', loc = 'lower right')
+    ax1.grid(True, axis = 'x')
+    ax2.grid(True, axis = 'x')
+    ax1.set(title = 'McMurdo Forecast %s' % today)
+
+    fig.autofmt_xdate() #All this is doing is rotating the tick lables
+    fig.set_size_inches(8,6)
+    fig.tight_layout()
+
+    fig.savefig(path+'mcm_tbl_plot.png')
+
+if __name__=='main':
+    print('main!')
+    path = '/home/markd/www/static/images/'
+#    path = 'C:\\Users\\owner\\Documents\\python\\PA_Get_AMPS\\'
+    file = '10km.mcm.table.txt'
+    today = dati.datetime.utcnow().strftime('%Y%m%d')
+
+    tbl_plot(path, file, today)
+
